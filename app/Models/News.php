d16ca6b79714a4aa3020fa2439220f947477ec93
vagrant <vagrant@homestead>
2020-08-28 19:18:03 +0000

    minor updates

--- app/Models/News.php
+++ app/Models/News.php
@@ -14,15 +14,4 @@ class News extends Model
     public function categories() {
         return $this->belongsTo(Category::class);
     }
-
-    public function getByCategory(int $id) {
-        return \DB::select("
-            SELECT n.id, n.title, n.author, n.body, c.name
-                FROM news n
-                LEFT JOIN categories c
-                ON n.category_id = c.id
-            WHERE category_id= :category_id", [
-            'category_id' => $id
-        ]);
-    }
 }
